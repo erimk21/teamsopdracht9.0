ca32ef8ad7dbade49cf06283aa0d113191016abc
erimk21 <9019259@student.zadkine.nl>
2023-02-18 21:27:21 +0100

    opzet 9

---
+++ opdracht9phpteamsopdr/teamsopdracht/functions1.php
@@ -0,0 +1,33 @@
+<?php
+
+function OpenDb(){
+    $db = new PDO('mysql:host=localhost;dbname=bieren', 'root', '');
+ }   
+
+
+function OvzBieren(){
+   $conn = openDb();
+}
+
+
+
+
+
+
+function connectDb(){
+    echo "Function DoeIets<br>";
+}
+
+// $servername = "localhost";
+//     $username = "root";
+//     $password = "";
+//     $dbname = "bieren";
+//   }
+
+
+
+
+
+
+?>
+
